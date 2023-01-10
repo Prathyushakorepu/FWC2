26de13b78597f2e849efb288d636eadeeb0aa5e7
Prathyushakorepu <111723616+Prathyushakorepu@users.noreply.github.com>
2023-01-10 12:04:31 +0530

    Add files via upload

--- Digital-communications/codes/G-to-other/vcdf.py
+++ Digital-communications/codes/G-to-other/vcdf.py
@@ -2,38 +2,48 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-#if using termux
-import subprocess
-import shlex
-#end if
-
-
-
-x = np.linspace(0,10,30)#points on the x axis
+maxrange=50
+maxlim=300.0
+x = np.linspace(0,maxlim,maxrange)    #points on the x axis
 simlen = int(1e6) #number of samples
 err = [] #declaring probability list
-#randvar = np.random.normal(0,1,simlen)
-randvar = np.loadtxt('v.dat',dtype='double')
-#randvar = np.loadtxt('gau.dat',dtype='double')
-for i in range(0,30):
-	err_ind = np.nonzero(randvar < x[i]) #checking probability condition
+pdf = [] #declaring pdf list
+h = 2*maxlim/(maxrange-1);
+x1_var = np.random.normal(0, 1, simlen)
+x2_var = np.random.normal(0, 1, simlen)
+v_var = x1_var**2 + x2_var**2
+
+for i in range(0,maxrange):
+	err_ind = np.nonzero(v_var < x[i]) #checking probability condition
 	err_n = np.size(err_ind) #computing the probability
 	err.append(err_n/simlen) #storing the probability values in a list
-
 	
-plt.plot(x.T,err)#plotting the CDF
-plt.grid() #creating the grid
+pdf = np.gradient(err, x, edge_order=2)
+a=(1/2)*np.exp(-x/2)
+b=1-np.exp(-x/2)
+plt.plot(x.T,err,'orange')   #plotting the CDF
+y=np.piecewise(x,[x<0,x>=0],[0,lambda a:a])
+plt.scatter(y,err)
+plt.grid()          #creating the grid
 plt.xlabel('$x$')
 plt.ylabel('$F_X(x)$')
+plt.legend(["Numerical","Theory"])
 
-#if using termux
 plt.savefig('/home/chinni/digitalc/figs/v_cdf.pdf')
-#plt.savefig('../figs/uni_cdf.eps')
-#subprocess.run(shlex.split("termux-open ../figs/uni_cdf.pdf"))
-#if using termux
-#plt.savefig('../figs/gauss_cdf.pdf')
-#plt.savefig('../figs/gauss_cdf.eps')
-#subprocess.run(shlex.split("termux-open ../figs/gauss_cdf.pdf"))
-#else
-plt.show() #opening the plot window
+#plt.savefig('../../figs/ch4/chisq_cdf.png')
+
+plt.show()
+plt.close()
+z=np.piecewise(x,[x<0,x>=0],[0,lambda b:b])
+plt.scatter(z,pdf)
+plt.plot(x,pdf,'orange')             # plotting estimated PDF
+plt.grid() #creating the grid
+plt.xlabel('$x_i$')
+plt.ylabel('$p_X(x_i)$')
+plt.legend(["Numerical","Theory"])
+plt.savefig('/home/chinni/digitalc/figs/v_pdf.pdf')
+
+plt.show()
+
+
 
